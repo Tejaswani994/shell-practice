2dd356b4324c2c4bd777369971dd598fd2f9acb5
Tejaswani994 <tejaswani.muthyala@gmail.com>
2026-03-02 17:01:49 +0530

    shell-practice

---
+++ 10-colors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo -e "\e[31m hello world \e[0m"
+echo -e "\e[32m hello world \e[0m"
+echo -e "\e[33m hello world \e[0m"
+echo -e "\e[34m hello world \e[0m"
+echo -e "\e[35m hello world \e[0m"
+echo -e "\e[36m hello world \e[0m"
+echo -e "\e[37m hello world \e[0m"
+echo -e "\e[41m hello world \e[0m"
+echo -e "\e[42m hello world \e[0m"
+echo -e "\e[43m hello world \e[0m"
+echo -e "\e[44m hello world \e[0m"
+echo -e "\e[45m hello world \e[0m"
+echo -e "\e[46m hello world \e[0m"
+echo -e "\e[47m hello world \e[0m"
+
+
+#OUTPUT: 
